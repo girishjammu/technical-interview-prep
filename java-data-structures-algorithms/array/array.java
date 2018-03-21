1150f140471152788962aff223a9d4f7e7cd9998
Girish Jammu <Girish.Jammu@fmr.com>
2018-03-21 10:46:01 -0400

    array

---
+++ java-data-structures-algorithms/array/array.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class array{
+    public static void main(String args[]){
+        // inatilize a new array with a fixed value
+        int[] myArr = new int[3];
+        // set array at 0 index
+        myArr[0] = 10;
+        // print value of array in 0 index
+        System.out.println(myArr[0]);
+    }
+}
